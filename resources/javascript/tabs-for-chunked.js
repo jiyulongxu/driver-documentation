03bfb896c55cf035302c5d2c7b04255b4cde6da4
mariascharin <maria.scharin@neotechnology.com>
2017-12-04 15:26:54 +0100

    Separate section with introduction blurb

--- resources/javascript/tabs-for-chunked.js
+++ resources/javascript/tabs-for-chunked.js
@@ -2,9 +2,7 @@ function tabTheSource($content) {
     var storedLanguage = getCodeExampleLanguage();
     var LANGUAGES = {
         'dotnet': 'C#',
-        'dotnet-async': 'C# Async',
         'java': 'Java',
-        'java-async': 'Java Async',
         'javascript': 'JavaScript',
         'python': 'Python'
     };
